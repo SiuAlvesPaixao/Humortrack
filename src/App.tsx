0ff2943550134281124270af6fd49261633033fb
SiuAlvesPaixao <77674342+SiuAlvesPaixao@users.noreply.github.com>
2025-11-17 21:57:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MoodForm from "./components/MoodForm";
+import HistoryChart from "./components/HistoryChart";
+import { exportCsv } from "./utils/exportCsv";
+
 
 function App() {
   const [count, setCount] = useState(0)
